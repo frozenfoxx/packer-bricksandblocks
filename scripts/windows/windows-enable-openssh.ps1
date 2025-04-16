9c2d7daf187a8b45feee9c2a85c1d0414f42df7a
FrozenFOXX <frozenfoxx@cultoffoxx.net>
2025-04-16 13:52:42 -0700

    Added new Windows scripts

---
+++ scripts/windows/windows-enable-openssh.ps1
@@ -0,0 +1,33 @@
+<#
+	.DESCRIPTION
+	Installs and configures OpenSSH
+
+	.SYNOPSIS
+	- Adds the capability for OpenSSH.Server
+	- From url: https://docs.ansible.com/ansible/latest/os_guide/windows_ssh.html
+#>
+
+Get-WindowsCapability -Name OpenSSH.Server* -Online |
+    Add-WindowsCapability -Online
+Set-Service -Name sshd -StartupType Automatic -Status Running
+
+$firewallParams = @{
+    Name        = 'sshd-Server-In-TCP'
+    DisplayName = 'Inbound rule for OpenSSH Server (sshd) on TCP port 22'
+    Action      = 'Allow'
+    Direction   = 'Inbound'
+    Enabled     = 'True'  # This is not a boolean but an enum
+    Profile     = 'Any'
+    Protocol    = 'TCP'
+    LocalPort   = 22
+}
+New-NetFirewallRule @firewallParams
+
+$shellParams = @{
+    Path         = 'HKLM:\SOFTWARE\OpenSSH'
+    Name         = 'DefaultShell'
+    Value        = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe'
+    PropertyType = 'String'
+    Force        = $true
+}
+New-ItemProperty @shellParams
